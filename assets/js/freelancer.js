e7f882dca3d52a9f8536d8bfc6f3abbbd50baf88
Aaron Jetro Alvarez <aaronjetro.alvarez.cics@ust.edu.ph>
2024-10-25 02:04:21 +0800

    Update freelancer.js

--- assets/js/freelancer.js
+++ assets/js/freelancer.js
@@ -4,12 +4,15 @@
   var envelopeWrappers = document.querySelectorAll('.custom-envelope-wrapper');
 
   envelopeWrappers.forEach(function(wrapper) {
-    wrapper.addEventListener('click', function() {
+    wrapper.parentElement.addEventListener('click', function(event) {
+      // Prevent the default action of the anchor tag
+      event.preventDefault();
+
       // Trigger the envelope opening animation
       wrapper.classList.add('open-envelope');
 
-      // Get the corresponding modal ID from the parent anchor tag's dataset
-      var modalId = this.parentElement.getAttribute('data-modal');
+      // Get the corresponding modal ID from the anchor tag's dataset
+      var modalId = this.getAttribute('data-modal');
 
       // Wait for the animation to complete before triggering the modal
       setTimeout(function() {
